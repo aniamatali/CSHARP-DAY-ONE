b7451a1b513a950d12f831f23b4c2b33480c27d1
Alvin Niamatali and Hansen Hansen <alvin+hansen@>
2017-10-09 10:03:00 -0700

    done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/")]
+    public ActionResult Hello()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient("Jessica");
+      myLetterVariable.SetSender("John");
+      return View(myLetterVariable);
+    }
+
+    [Route("/favorite_photos")]
+    public ActionResult FavoritePhotos()
+    {
+      return View();
+    }
+
+    [Route("/form")]
+    public ActionResult Form()
+    {
+      return View();
+    }
+
+    [Route("/greeting_card")]
+        public ActionResult GreetingCard()
+        {
+            LetterVariable myLetterVariable = new LetterVariable();
+            myLetterVariable.SetRecipient(Request.Query["recipient"]);
+            myLetterVariable.SetSender(Request.Query["sender"]);
+            return View("Hello", myLetterVariable);
+        }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+
+  }
+}
